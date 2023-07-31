00470cda60ad852e96acccf9d5a4bdb2a963f94e
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-07-31 08:32:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const extendHex = (shortHex) => {
-  // write your code here
+	let s = "";
+	for (let i=0; i<shortHex.length; i++) {
+		if (shortHex.charAt(i) !== '#') {
+			let ct = 2;
+			
+			while (ct > 0) {
+				s += shortHex.charAt(i);
+				ct--;
+			}
+		}
+	}
+	return '#' + s;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
+const shortHex = prompt("Enter Short Hex.")
 alert(extendHex(shortHex));
